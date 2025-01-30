5053bba39e0c9a7edbc7dc300d54210e8f2a210b
Oleg Ten <tennet0505@gmail.com>
2025-01-30 23:44:24 +0100

    icons

--- lib/presentation/maps/map.dart
+++ lib/presentation/maps/map.dart
@@ -1,7 +1,13 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:real_estate_app/data/models/house.dart';
+import 'package:real_estate_app/presentation/helpers/app_images.dart';
 import 'package:real_estate_app/presentation/maps/widgets/poi_detail_widget.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
 
 class MapScreen extends StatefulWidget {
   final List<House> houses;
@@ -17,15 +23,58 @@ class _MapScreenState extends State<MapScreen>
   late GoogleMapController mapController;
   final LatLng _initialPosition = const LatLng(52.3676, 4.9041); // Amsterdam
   Set<Marker> _markers = {};
+  BitmapDescriptor? _customIcon;
 
   @override
   void initState() {
     super.initState();
     _loadMarkers();
+    _loadCustomMarker();
+  }
+
+  Future<void> _loadCustomMarker() async {
+    final customIcon = await _getBytesFromAsset(AppImages.logo, 24);
+    setState(() {
+      _customIcon = BitmapDescriptor.bytes(customIcon);
+      _updateMarkers(); // Update markers after loading icon
+    });
+  }
+
+  Future<Uint8List> _getBytesFromAsset(String path, int width) async {
+    final ByteData data = await rootBundle.load(path);
+    final ui.Codec codec = await ui.instantiateImageCodec(
+      data.buffer.asUint8List(),
+      targetWidth: width,
+    );
+    final ui.FrameInfo fi = await codec.getNextFrame();
+    final ByteData? byteData =
+        await fi.image.toByteData(format: ui.ImageByteFormat.png);
+    return byteData!.buffer.asUint8List();
+  }
+
+  void _updateMarkers() {
+    if (_customIcon == null) return;
+    setState(() {
+      _markers = widget.houses
+          .map((house) => Marker(
+                markerId: MarkerId(house.id.toString()),
+                position: LatLng(house.latitude, house.longitude),
+                icon: _customIcon!, // Apply the custom marker icon
+                onTap: () {
+                  _showPoiBottomSheet(context, house);
+                },
+                infoWindow: InfoWindow(
+                  title: house.zip,
+                  snippet: '€${house.price.toString()}',
+                ),
+              ))
+          .toSet();
+    });
   }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    _updateMarkers();
   }
 
   @override
